329c8bd88b6c25b0d117cf76b4997afd4050e25e
Vlad Skvortsov <vss@73rus.com>
2017-05-14 14:57:47 -0700

    Accept tag as an optional parameter

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #! /bin/sh -e
 
-docker build -t wadcom/youtube2podcast .
+if [ $# -eq 0 ]; then
+    TAG=latest
+elif [ $# -eq 1 ]; then
+    TAG=$1
+else
+    echo "Usage: $0 [TAG]" >&2
+    exit 1
+fi
+
+docker build -t wadcom/youtube2podcast:$TAG .
